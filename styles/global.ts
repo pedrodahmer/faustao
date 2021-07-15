442da4b647e8dc7146d1121216423cc0a3888865
Pedro Dahmer <pedrohd1213@gmail.com>
2021-07-15 17:50:44 -0300

    first commit

---
+++ styles/global.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100vw;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
